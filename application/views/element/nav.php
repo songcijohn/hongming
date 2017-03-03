54ddd9147680aa717c7e8b26a442b4b23780d3e6
songci <songci@lightinthebox.com>
2017-03-03 18:37:57 +0800

    修改样式

--- application/views/element/nav.php
+++ application/views/element/nav.php
@@ -4,12 +4,6 @@
         <div class="row">
             <!-- Logo start -->
             <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
                 <div class="navbar-brand">
                     <a href="/">
                         <img class="img-responsive" src="/public/images/logo.png" alt="logo">
@@ -18,8 +12,8 @@
             </div><!--/ Logo end -->
             <nav class="collapse navbar-collapse clearfix" role="navigation">
                 <ul class="nav navbar-nav navbar-right">
-                    <li class="dropdown <?php if($this->uri->uri_string == 'about'){ ?> active <?php }?>">
-                        <a href="/about" class="dropdown-toggle" data-toggle="dropdown">关于宏明 <i class="fa fa-angle-down"></i></a>
+                    <li class="dropdown <?php if(in_array($this->uri->uri_string, array('about','about/advantage','about/history'))){ ?> active <?php }?>">
+                        <a href="/about" class="dropdown-toggle">关于宏明 <i class="fa fa-angle-down"></i></a>
                         <div class="dropdown-menu">
                             <ul>
                                 <li <?php if($this->uri->uri_string == 'about'){ ?>class="active" <?php }?>><a href="/about/index">宏明简介</a></li>
@@ -30,8 +24,8 @@
                     </li>
                     <li <?php if($this->uri->uri_string == 'hongming/product'){ ?>class="active" <?php }?>><a href="/hongming/product">产品中心</a></li>
                     <li <?php if($this->uri->uri_string == 'hongming/news'){ ?>class="active" <?php }?>><a href="/hongming/news">新闻动态</a></li>
-                    <li class="dropdown <?php if($this->uri->uri_string == 'contact'){ ?> active <?php }?>">
-                        <a href="/contact" class="dropdown-toggle" data-toggle="dropdown">联系我们 <i class="fa fa-angle-down"></i></a>
+                    <li class="dropdown <?php if(in_array($this->uri->uri_string, array('contact','contact/hr'))){ ?> active <?php }?>">
+                        <a href="/contact" class="dropdown-toggle">联系我们 <i class="fa fa-angle-down"></i></a>
                         <div class="dropdown-menu">
                             <ul>
                                 <li <?php if($this->uri->uri_string == 'contact/hr'){ ?>class="active" <?php }?>><a href="/contact/hr">人才招聘</a></li>
