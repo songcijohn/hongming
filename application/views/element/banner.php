fa29f5f38c5973df33a60474f2ca466044b2f66c
e1988117 <21295124@qq.com>
2017-03-07 16:25:08 +0800

    图片

--- application/views/element/banner.php
+++ application/views/element/banner.php
@@ -12,16 +12,47 @@
                     <div class="flex-caption slider-content">
                         <div class="col-md-12 text-center">
                             <h2 class="animated2">
-                                Need To Invent The Future!
+                                为国护航，我们初心不改
                             </h2>
                             <h3 class="animated3">
-                                We Making Difference To Great Things Possible
+                                We Making Difference To Our Space Industry
                             </h3>
-                            <p class="animated4"><a href="#" class="slider btn btn-primary white">Check Now</a></p>
+                            <p class="animated4"><a href="#" class="slider btn btn-primary white">行业应用</a></p>
                         </div>
                     </div>
                 </li>
-                
+                <li>
+                    <div class="overlay2">
+                        <img class="" src="/public/images/slider/radar.jpg" alt="slider">
+                    </div>
+                    <div class="flex-caption slider-content">
+                        <div class="col-md-12 text-center">
+                            <h2 class="animated2">
+                                国之重器，宏明军工情
+                            </h2>
+                            <h3 class="animated3">
+                                We Making Difference To Our Military Industry
+                            </h3>
+                            <p class="animated4"><a href="#" class="slider btn btn-primary white">行业应用</a></p>
+                        </div>
+                    </div>
+                </li>
+				<li>
+					<div class="overlay2">
+						<img class="" src="/public/images/slider/volvo.jpg" alt="slider">
+					</div>
+					<div class="flex-caption slider-content">
+	                       <div class="col-md-12 text-center">
+	                    	<h2 class="animated2">
+	                        	汽车&通讯，科技创新为先
+	                        </h2>
+	                           <h3 class="animated3">
+	                            We Making Difference To Our Daily Life
+	                           </h3>
+	                           <p class="animated4"><a href="#" class="slider btn btn-primary white">行业应用</a></p>
+	                       </div>
+	                </div>
+				</li>
             </ul>
         </div>
     </div><!--/ Main slider end -->    	
