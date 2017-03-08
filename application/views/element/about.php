474cfa352ddfdf23b44f1bc1acb81070065ffc05
songci <songci@lightinthebox.com>
2017-03-08 16:26:15 +0800

    修改样式

--- application/views/element/about.php
+++ application/views/element/about.php
@@ -47,27 +47,27 @@
                 </ul>
                 <div class="tab-content col-md-9 col-sm-7">
                     <div class="tab-pane active animated fadeInRight" id="tab_a">
-                        <i class="fa fa-trophy big"></i>
+                        <img class="about-index-img" src="/public/images/blog/author.jpg" />
                         <h3>We Are Awwared Winning Company</h3> 
                         <p>Over the year we have lots of experience in our field. In sit amet massa sapien. Vestibulum diam turpis, gravida in lobortis id, ornare a eros. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sagittis nulla non elit dignissim suscipit. Duis lorem nulla, eleifend.</p>
                     </div>
                     <div class="tab-pane animated fadeInLeft" id="tab_b">
-                        <i class="fa fa-briefcase big"></i>
+                        <img class="about-index-img" src="/public/images/blog/author.jpg" />
                         <h3>We Have Worldwide Business</h3> 
                         <p>Helvetica cold-pressed lomo messenger bag ugh. Vinyl jean shorts Austin pork belly PBR retro, Etsy VHS kitsch actually messenger bag pug. Pbrb semiotics try-hard, Schlitz occupy dreamcatcher master cleanse Marfa Vice tofu. </p>							 
                     </div>
                     <div class="tab-pane animated fadeIn" id="tab_c">
-                        <i class="fa fa-android big"></i>
+                        <img class="about-index-img" src="/public/images/blog/author.jpg" />
                         <h3>We Build Readymade Applications</h3> 
                         <p>Over the year we have lots of experience in our field. In sit amet massa sapien. Vestibulum diam turpis, gravida in lobortis id, ornare a eros. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sagittis nulla non elit dignissim suscipit. Duis lorem nulla, eleifend.</p>
                     </div>
                     <div class="tab-pane animated fadeIn" id="tab_d">
-                        <i class="fa fa-pagelines big"></i>
+                        <img class="about-index-img" src="/public/images/blog/author.jpg" />
                         <h3>Clean and Modern Design</h3> 
                         <p>Over the year we have lots of experience in our field. In sit amet massa sapien. Vestibulum diam turpis, gravida in lobortis id, ornare a eros. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sagittis nulla non elit dignissim suscipit. Duis lorem nulla, eleifend.</p>
                     </div>
                     <div class="tab-pane animated fadeIn" id="tab_e">
-                        <i class="fa fa-support big"></i>
+                        <img class="about-index-img" src="/public/images/blog/author.jpg" />
                         <h3>24/7 Dedicated Support</h3> 
                         <p>Occupy selfies Tonx, +1 Truffaut beard organic normcore tilde flannel artisan squid cray single-origin coffee. Master cleanse vinyl Austin kogi, semiotics skateboard fap wayfarers health goth. Helvetica cray church-key hashtag Carles. Four dollar toast meggings seitan, Tonx pork belly VHS Bushwick. Chambray banh mi cornhole. Locavore messenger bag seitan.</p>
                     </div>
--- application/views/element/parallax_2.php
+++ application/views/element/parallax_2.php
@@ -7,8 +7,8 @@
                 <h2>宏明在各个时期都受到国家各级领导的关注和关怀</h2>
                 <h3>是厚爱，更是鼓舞和鞭策</h3>
                 <p>
-                    <a href="#" class="btn btn-primary white">宏明简介</a>
-                    <a href="#" class="btn btn-primary solid">宏明历程</a>
+                    <a href="/about/index" class="btn btn-primary white">宏明简介</a>
+                    <a href="/about/history" class="btn btn-primary solid">宏明历程</a>
                 </p>
             </div>
         </div>
